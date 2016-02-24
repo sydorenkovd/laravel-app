7666f2314179cdac4cb386fca6a672663f9e1e07
sydorenkovd <sydorenkovd@gmail.com>
2016-02-24 15:52:48 +0200

    laravel app

---
+++ app/Http/Controllers/GalleryController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class GalleryController extends Controller
+{
+    public function index(){
+    	die('GALLERY/INDEX');
+    }
+    public function create(){
+    	die('GALERRY/CREATE');
+    }
+}
