4cd6233ba47c0d1285464e02666210d8f9f12186
sydorenkovd <sydorenkovd@gmail.com>
2016-05-22 12:03:23 +0300

    some refactoring code

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -1,5 +1,84 @@
 @extends('layouts.main')
 @section('content')
-Gallery/Index
-{{$test}}
+    <div class="callout primary">
+        <div class="row column">
+            <h1>Photo galleries</h1>
+            <p class="lead">Create a gallery and start uploading</p>
+        </div>
+    </div>
+    <div class="row small-up-2 medium-up-3 large-up-4">
+        <div class="column">
+            <img class="thumbnail" src="http://placehold.it/550x550">
+            <h5>My Site</h5>
+        </div>
+        <div class="column">
+            <img class="thumbnail" src="http://placehold.it/550x550">
+            <h5>My Site</h5>
+        </div>
+        <div class="column">
+            <img class="thumbnail" src="http://placehold.it/550x550">
+            <h5>My Site</h5>
+        </div>
+        <div class="column">
+            <img class="thumbnail" src="http://placehold.it/550x550">
+            <h5>My Site</h5>
+        </div>
+        <div class="column">
+            <img class="thumbnail" src="http://placehold.it/550x550">
+            <h5>My Site</h5>
+        </div>
+        <div class="column">
+            <img class="thumbnail" src="http://placehold.it/550x550">
+            <h5>My Site</h5>
+        </div>
+        <div class="column">
+            <img class="thumbnail" src="http://placehold.it/550x550">
+            <h5>My Site</h5>
+        </div>
+        <div class="column">
+            <img class="thumbnail" src="http://placehold.it/550x550">
+            <h5>My Site</h5>
+        </div>
+        <div class="column">
+            <img class="thumbnail" src="http://placehold.it/550x550">
+            <h5>My Site</h5>
+        </div>
+        <div class="column">
+            <img class="thumbnail" src="http://placehold.it/550x550">
+            <h5>My Site</h5>
+        </div>
+        <div class="column">
+            <img class="thumbnail" src="http://placehold.it/550x550">
+            <h5>My Site</h5>
+        </div>
+        <div class="column">
+            <img class="thumbnail" src="http://placehold.it/550x550">
+            <h5>My Site</h5>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="medium-6 columns">
+            <h3>Contact Me</h3>
+            <p>Vivamus hendrerit arcu sed erat molestie vehicula. Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor. Suspendisse dictum feugiat nisl ut dapibus. Mauris iaculis porttitor.</p>
+            <ul class="menu">
+                <li><a href="#">Dribbble</a></li>
+                <li><a href="#">Facebook</a></li>
+                <li><a href="#">Yo</a></li>
+            </ul>
+        </div>
+        <div class="medium-6 columns">
+            <label>Name
+                <input type="text" placeholder="Name">
+            </label>
+            <label>Email
+                <input type="text" placeholder="Email">
+            </label>
+            <label>
+                Message
+                <textarea placeholder="holla at a designerd"></textarea>
+            </label>
+            <input type="submit" class="button expanded" value="Submit">
+        </div>
+    </div>
     @stop
