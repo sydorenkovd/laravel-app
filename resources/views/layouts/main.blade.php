12a7eaeb5f8b6cc21589a3cc868f152224ca8ff6
sydorenkovd <sydorenkovd@gmail.com>
2016-05-22 11:31:42 +0300

    portfolio

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,24 +1,120 @@
 <!doctype html>
-<html>
+<html class="no-js" lang="en">
 <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="/css/bootstrap.css">
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Foundation | Welcome</title>
+    <link rel="stylesheet" href="/css/foundation.css">
     <link rel="stylesheet" href="/css/app.css">
-    <title>Photo Galery | Welcome</title>
 </head>
 <body>
-<ul class="list-group ">
-    <li class="list-group-info"><a href="/">Home</a></li>
-    <li class="list-group-item-info"><a href="login">Login</a></li>
-    <li class="list-group-item-info"><a href="register">Register</a></li>
-    <li class="list-group-item-info"><a href="/gallery/create">Create gallery</a></li>
-</ul>
-
-
-<div>
-    @yield('content')
+<div class="off-canvas-wrapper">
+    <div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
+        <div class="off-canvas position-left reveal-for-large" id="my-info" data-off-canvas data-position="left">
+            <div class="row column">
+                <br>
+                <img class="thumbnail" src="http://placehold.it/550x350">
+                <h5>Mike Mikerson</h5>
+                <p>Duis aliquet egestas purus in blandit. Curabitur vulputate, ligula lacinia scelerisque tempor, lacus lacus ornare ante, ac egestas est urna sit amet arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed molestie augue sit amet leo.</p>
+            </div>
+        </div>
+        <div class="off-canvas-content" data-off-canvas-content>
+            <div class="title-bar hide-for-large">
+                <div class="title-bar-left">
+                    <button class="menu-icon" type="button" data-open="my-info"></button>
+                    <span class="title-bar-title">Mike Mikerson</span>
+                </div>
+            </div>
+            <div class="callout primary">
+                <div class="row column">
+                    <h1>Hello! This is the portfolio of a very witty person.</h1>
+                    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla.</p>
+                </div>
+            </div>
+            <div class="row small-up-2 medium-up-3 large-up-4">
+                <div class="column">
+                    <img class="thumbnail" src="http://placehold.it/550x550">
+                    <h5>My Site</h5>
+                </div>
+                <div class="column">
+                    <img class="thumbnail" src="http://placehold.it/550x550">
+                    <h5>My Site</h5>
+                </div>
+                <div class="column">
+                    <img class="thumbnail" src="http://placehold.it/550x550">
+                    <h5>My Site</h5>
+                </div>
+                <div class="column">
+                    <img class="thumbnail" src="http://placehold.it/550x550">
+                    <h5>My Site</h5>
+                </div>
+                <div class="column">
+                    <img class="thumbnail" src="http://placehold.it/550x550">
+                    <h5>My Site</h5>
+                </div>
+                <div class="column">
+                    <img class="thumbnail" src="http://placehold.it/550x550">
+                    <h5>My Site</h5>
+                </div>
+                <div class="column">
+                    <img class="thumbnail" src="http://placehold.it/550x550">
+                    <h5>My Site</h5>
+                </div>
+                <div class="column">
+                    <img class="thumbnail" src="http://placehold.it/550x550">
+                    <h5>My Site</h5>
+                </div>
+                <div class="column">
+                    <img class="thumbnail" src="http://placehold.it/550x550">
+                    <h5>My Site</h5>
+                </div>
+                <div class="column">
+                    <img class="thumbnail" src="http://placehold.it/550x550">
+                    <h5>My Site</h5>
+                </div>
+                <div class="column">
+                    <img class="thumbnail" src="http://placehold.it/550x550">
+                    <h5>My Site</h5>
+                </div>
+                <div class="column">
+                    <img class="thumbnail" src="http://placehold.it/550x550">
+                    <h5>My Site</h5>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="medium-6 columns">
+                    <h3>Contact Me</h3>
+                    <p>Vivamus hendrerit arcu sed erat molestie vehicula. Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor. Suspendisse dictum feugiat nisl ut dapibus. Mauris iaculis porttitor.</p>
+                    <ul class="menu">
+                        <li><a href="#">Dribbble</a></li>
+                        <li><a href="#">Facebook</a></li>
+                        <li><a href="#">Yo</a></li>
+                    </ul>
+                </div>
+                <div class="medium-6 columns">
+                    <label>Name
+                        <input type="text" placeholder="Name">
+                    </label>
+                    <label>Email
+                        <input type="text" placeholder="Email">
+                    </label>
+                    <label>
+                        Message
+                        <textarea placeholder="holla at a designerd"></textarea>
+                    </label>
+                    <input type="submit" class="button expanded" value="Submit">
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-<script src="/js/jquery-1.11.1.min.js"></script>
-<script src="/js/bootstrap.min.js"></script>
+<script src="js/vendor/foundation.js"></script>
+<script src="js/vendor/jquery.js"></script>
+{{--<script src="js/vendor/what-input.js"></script>--}}
+<script src="js/app.js"></script>
+<script>
+    $(document).foundation();
+</script>
 </body>
 </html>
