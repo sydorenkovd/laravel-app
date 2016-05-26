d0c2dcbbf3afd128765a7bc66047713526c77f86
sydorenkovd <sydorenkovd@gmail.com>
2016-05-26 20:15:13 +0300

    auth

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,13 +10,13 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
 Route::get('/',  'GalleryController@index');
 Route::resource('gallery', 'GalleryController');
 Route::resource('photo', 'PhotoController');
 Route::get('/gallery/show/{id}', 'GalleryController@show');
 Route::get('/photo/create/{id}', 'PhotoController@create');
 Route::get('/photo/details/{id}', 'PhotoController@details');
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -31,3 +31,7 @@ Route::get('/photo/details/{id}', 'PhotoController@details');
 Route::group(['middleware' => ['web']], function () {
     //
 });
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
