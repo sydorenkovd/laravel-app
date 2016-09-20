f003075ede267e422f1f906b2f26ee36c6da3785
sydorenkovd <sydorenkovd@gmail.com>
2016-09-20 22:20:39 +0300

    gulp compile scss

---
+++ tests/UserTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->visit('/')->click('All Photos')
+         ->seePageIs('/photo')
+         ->see('sydorenkov');
+    }
+}
