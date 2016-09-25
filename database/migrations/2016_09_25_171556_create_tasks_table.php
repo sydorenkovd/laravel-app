024bd28a22cf6b17ed625c8bd9094e3da8e08bae
sydorenkovd <sydorenkovd@gmail.com>
2016-09-25 20:45:28 +0300

    files for relations

---
+++ database/migrations/2016_09_25_171556_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('photo_id')->unsigned();
+            $table->foreign('photo_id')
+                ->references('id')->on('photos')
+                ->onDelete('cascade');
+            $table->string('name');
+            $table->text('description');
+            $table->boolean('done')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tasks');
+    }
+}
---
+++ database/migrations/2016_09_25_172858_rename_photo.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RenamePhoto extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function ($table) {
+            $table->renameColumn('photo_id', 'photos_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
